506ba7317452d3abd5ab2b34a12e830986f6ece3
Rodrigo <rgtzrbl@gmail.com>
2024-01-26 00:21:09 +0100

    Adicion de CORS para dominio del frontend

--- src/app.js
+++ src/app.js
@@ -11,6 +11,14 @@ import bcrypt from 'bcrypt';
 const app = express();
 app.use(express.json());
 app.use(cors());
+//Permitimos la conexión desde el frontend
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://nsxlover.github.io/pruebaFrontEnd.github.io/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("Realice un ping para comprobar si existe una conexión");
